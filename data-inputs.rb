5eca1be79d625e155b337791d6f775739cf9394f
Felipe Ribeiro <piporibeiro@gmail.com>
2021-11-06 19:32:47 -0300

    hash and data inputs methods

---
+++ data-inputs.rb
@@ -0,0 +1,12 @@
+# Entrada de dados
+
+# usamos para isso gets
+puts  "Digite seu nome: "
+ name = gets
+
+puts "Seu nome é #{name}"
+
+
+# chomp() remove o último caractere especial
+
+name.chomp
---
+++ hashes.rb
@@ -0,0 +1,12 @@
+# hashes é uma estrutura de chave e valor
+# A chave desse hash é um objeto
+
+h = {}
+h[:name] = "Felipe"
+h[:age] = 38
+
+p h
+p h[:name]
+p h.keys
+p h.values
+p h.empty?
